ace6aef704b57408900ceb15d4544218501380be
dacxiaoz <2495308304@qq.com>
2021-01-19 16:10:29 +0800

    前台

--- react_blog/blog/config/apiUrl.js
+++ react_blog/blog/config/apiUrl.js
@@ -3,7 +3,8 @@ let ipUrl = 'http://127.0.0.1:7001/default/'
 let servicePath = {
     getArticlelist : ipUrl+'getArticlelist', //首页接口
     getArticleById : ipUrl+'getArticleById/',  //详细页接口
-    getTypeInfo : ipUrl+'getTypeInfo/',  //文章类别接口
+    getTypeInfo : ipUrl+'getTypeInfo',  //文章类别接口
+    getListById : ipUrl+'getListById/',  //根据类别ID获得文章列表
 }
 
 export default servicePath
--- react_blog/blog/pages/index.js
+++ react_blog/blog/pages/index.js
@@ -9,6 +9,10 @@ import {
   ReconciliationOutlined,
 } from '@ant-design/icons';
 
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime.css'
+
 import Author from '../components/Author'
 import Header from '../components/Header'
 import Advert from '../components/Advert'
@@ -17,6 +21,21 @@ import Footer from '../components/Footer'
 
 const Home=(list)=>{
   const [mylist,setMylist] = useState(list.data)
+  const renderer = new marked.Renderer()
+  
+  marked.setOptions({
+    renderer:renderer, //渲染的方式
+    gfm:true,  //启动类似github样式的makdown
+    pedantic:false,//容错
+    sanitize:false,//原始输入 忽略html标签
+    tables:true,    //是否允许输出github表格 gfm 为true时
+    breaks:false,  //是否支持换行符 gfm 为true时
+    smartLists:true,    //自动渲染我们的列表样式
+    highlight:function(code){ //代码高亮
+      return hljs.highlightAuto(code).value    
+    }
+  })
+
   return (
     <div>
       <Head>
@@ -41,7 +60,9 @@ const Home=(list)=>{
                       <span> <ReconciliationOutlined /> {item.typeName}</span>
                       <span> <ReconciliationOutlined /> {item.view_count}</span>
                       </div>
-                      <div className="context">{item.introduce}</div>
+                      <div className="context"
+                      dangerouslySetInnerHTML={{__html:marked(item.introduce)}}>
+                      </div>
                   </List.Item>
                 )}
               />
@@ -52,6 +73,47 @@ const Home=(list)=>{
           </Col>
       </Row>
       <Footer />
+      <style jsx>{`
+        pre{
+          display: block;
+          background-color: #283646 !important;
+           padding: .5rem !important;
+           overflow-y: auto;
+           font-weight: 300;
+           font-family: Menlo, monospace;
+           border-radius: .3rem;
+      }
+      
+      pre >code{
+          border:0px !important;
+          background-color: #283646 !important;
+          color:#FFF;
+      
+      }
+      code {
+          display: inline-block ;
+          background-color:#f3f3f3;
+          border:1px solid #fdb9cc;
+          border-radius:3px;
+          font-size: 12px;
+          padding-left: 5px;
+          padding-right: 5px;
+          color:#4f4f4f;
+          margin: 0px 3px;
+      
+      }
+      
+      .list-context img{
+         width:100% ;
+         border-radius:5px;
+         border:1px solid #f0f0f0;
+         max-width:1000px !important;
+         display: block;
+         margin:8px  auto ;
+      
+      }      
+      `}
+      </style>
     </div>
   )
 }
