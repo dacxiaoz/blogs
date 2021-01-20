5a437c119d04fadd58d60c23703a02a996505285
dacxiaoz <2495308304@qq.com>
2021-01-20 15:57:55 +0800

    什么

--- react_blog/admin/src/App.js
+++ react_blog/admin/src/App.js
@@ -1,7 +1,10 @@
-
+import React from 'react'
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
 function App() {
   return (
     <div>
+      <Button type="primary">Hello Word</Button>
     </div>
   );
 }
